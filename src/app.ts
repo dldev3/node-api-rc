5a5076125064b5f1c3825fbf2db5ebbe2a44e8aa
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-11-17 16:35:29 +0530

    routes done

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { healthRouter, calculatorRouter } from './routes';
+
+const app = express();
+const port = 3000;
+
+app.use('/health', healthRouter);
+app.use('/calculator', calculatorRouter);
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Application is running at http://localhost:${port}`);
+});
