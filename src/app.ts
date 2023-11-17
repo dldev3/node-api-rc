f259a5a96433ef95eea899390e8d68c7c66266cc
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-11-17 16:48:06 +0530

    middlewares done

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import { healthRouter, calculatorRouter } from './routes';
+import { logger } from './middlewares';
 
 const app = express();
 const port = 3000;
 
+app.use(logger);
+
 app.use('/health', healthRouter);
 app.use('/calculator', calculatorRouter);
-
-
-
-
-
-
 
 
 
